291ee210c8ce6557f6676b991dfebe50fae921ae
Dzianis Maroz <kapitan.frost@gmail.com>
2024-07-14 16:01:56 +0300

    it\s too hot temperature to think....

--- db_explorer.go
+++ db_explorer.go
@@ -6,6 +6,9 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
+	"path"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -15,15 +18,16 @@ import (
 
 const (
 	UnknownTableErr         = "unknown table"              // If unknown table was requested.
-	RecordNotFoungErr       = "record not found"           // No entry in table at all or by specified id found.
-	InvalidIDTypeErrParrern = "field %s have invalid type" // Client submitted invalid field or type
+	RecordNotFoungErr       = "record not found"           // No entries in table at all or by specified id found.
+	InvalidIDTypeErrParrern = "field %s have invalid type" // Client submited invalid field or type
 )
 
 type Response struct {
-	Err  string      `json:"error,omitempty"`
-	Resp interface{} `json:"response,omitempty"`
+	Err  string      `json:"error,omitempty"`    // any error representation
+	Resp interface{} `json:"response,omitempty"` // any content as response
 }
 
+// !!delete candidate
 type Column struct {
 	Field     string // name of column
 	Type      string
@@ -37,14 +41,20 @@ type TablesList struct {
 }
 
 type TableMetadata struct {
-	columnNames []string
-	columnTypes []*sql.ColumnType
+	ColumnNames []string
+	ColumnTypes []*sql.ColumnType
 }
 
 type DBExplorer struct {
-	db         *sql.DB  // database handler
-	tableNames []string //
+	db         *sql.DB // database handler
+	TableNames TablesList
 	metadata   map[string]TableMetadata
+}
+
+type Req struct {
+	table  string
+	id     int64
+	params url.Values
 }
 
 func Resp(content interface{}, e error) Response {
@@ -90,7 +100,7 @@ func (d *DBExplorer) getColumnMetadata(tableName string) error {
 		if err != nil {
 			return err
 		}
-		tableMetadata := TableMetadata{columnTypes: colTypes, columnNames: colNames}
+		tableMetadata := TableMetadata{ColumnTypes: colTypes, ColumnNames: colNames}
 		d.metadata[tableName] = tableMetadata
 	}
 	return nil
@@ -109,7 +119,7 @@ func NewDbExplorer(db *sql.DB) (*DBExplorer, error) {
 	if len(tablesNames) < 1 {
 		return nil, errors.New("no tables in database")
 	}
-	result.tableNames = tablesNames
+	result.TableNames = TablesList{Tables: tablesNames}
 	for _, tableName := range tablesNames {
 		// collect each table metadata and persist in parent struct
 		err := result.getColumnMetadata(tableName)
@@ -120,14 +130,14 @@ func NewDbExplorer(db *sql.DB) (*DBExplorer, error) {
 	return result, nil
 }
 
-// Return table names, resolved on initialization step.
-// We can suspect it to be static.
+// Return table names, resolved on initialization. We can suspect it to be static.
 func (d *DBExplorer) ListTables() []string {
-	return d.tableNames
+	return d.TableNames.Tables
 }
 
+// Simple request tracking to StdOut.
 func (d *DBExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf(":::: %s [%s] : %s {%#v}\n", time.Now().Format(time.DateTime), r.Method, r.URL.Path, r.URL.Query()) // simple request tracking to std::out
+	fmt.Printf(":::: %s [%s] : %s {%#v}\n", time.Now().Format(time.DateTime), r.Method, r.URL.Path, r.URL.Query())
 	d.route(w, r)
 }
 
@@ -147,21 +157,62 @@ func (d *DBExplorer) route(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// ------------------ parse resuqest params ---------------------
+
+func parse(r *http.Request) (presult *Req, err error) {
+	path := r.URL.Path
+	tokens := strings.Split(path, "/")
+	var tableName string
+	var id int64
+	if len(tokens) > 0 {
+		tableName = tokens[0]
+	}
+	if len(tokens) > 1 {
+		candiate, err := strconv.ParseInt(tokens[1], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		id = candiate
+	}
+
+	return &Req{table: tableName, id: id, params: r.URL.Query()}, nil
+}
+
+func (r *Req) isTableRequest() bool {
+	return r.table == ""
+}
+
+func getId(p string) (int64, bool) {
+	str := path.Base(p)
+	if str == "" {
+		return -1, false
+	}
+	id, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return -1, false
+	}
+	return id, true
+}
+
 // -------------------------------- Handlers --------------------------------------
 func (d *DBExplorer) handleGet(w http.ResponseWriter, r *http.Request) {
-	pathTokens := strings.Fields(strings.ReplaceAll(r.URL.Path, "/", ""))
-	fmt.Printf("path [%d] : %s\n", len(pathTokens), pathTokens)
+	req, err := parse(r)
+	if err != nil {
+		Reply(w, http.StatusInternalServerError, Resp(nil, err))
+		return
+	}
+	// fmt.Printf("path [%s] : %s\n", req., params)
+	// fmt.Println("path", path_, "base", path.Base(path_), "clear", path.Clean(path_))
 	switch {
-	case len(pathTokens) < 1:
+	case req.isTableRequest():
 		Reply(w, http.StatusOK, Resp(map[string][]string{"tables": d.ListTables()}, nil))
-	case len(pathTokens) == 1:
-		switch {
-		case strings.Contains(pathTokens[0], "?"):
-			//d.GetEntriesFrom(w, pathTokens)
-		default:
-			d.FindFrom(pathTokens)
-			Reply(w, http.StatusNotFound, Resp(nil, errors.New(UnknownTableErr)))
+	default:
+		status := http.StatusOK
+		result, err := d.query(req)
+		if err != nil {
+			status = http.StatusInternalServerError
 		}
+		Reply(w, status, Resp(result, err))
 	}
 }
 
@@ -202,5 +253,16 @@ func (d DBExplorer) FindFrom(params []string) ([]interface{}, error) {
 	case len(params) < 1:
 		return nil, errors.New("invalid")
 	}
+	return nil, nil
+}
+
+func (d *DBExplorer) query(r *Req) (result interface{}, err error) {
+	// resolve query type
+	// 1. all ftom table ?
+	// if p == path.Base(p) {
+
+	// }
+	// 2. single row by id ?
+	// 3. query by criteria ( limits)
 	return nil, nil
 }
